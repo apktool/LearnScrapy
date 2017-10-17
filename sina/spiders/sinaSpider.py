bbad2390d80a2d9f54ed5b45f6cce87fc72d7547
apktool <apktoolOo@gmail.com>
2017-10-18 07:44:21 +0800

    Get sina personall information successfully

--- sina/spiders/sinaSpider.py
+++ sina/spiders/sinaSpider.py
@@ -1,4 +1,6 @@
 import scrapy
+import json
+from sina.items import InformationItem
 
 
 class SinaSpider(scrapy.Spider):
@@ -18,7 +20,28 @@ class SinaSpider(scrapy.Spider):
             yield scrapy.Request(url=url, meta=response.meta['cookiejar'], callback=self.parse_information)
 
     def parse_information(self, response):
-        filename = 'sina.json'
-        with open(filename, 'wb') as f:
-            f.write(response.body)
-        self.log('Saved file %s' % filename)
+        text = response.body.decode('unicode_escape')
+        content = json.loads(text)
+        meta_data = dict()
+
+        for card in content['cards']:
+            for card_group in card['card_group']:
+                key = card_group.get('item_name', '微博认证')
+                value = card_group.get('item_content')
+                meta_data[key] = value
+
+        info_items = InformationItem()
+        info_items['_id'] = '2302832185608961'
+        info_items['nickname'] = meta_data.get('昵称')
+        info_items['authorization'] = meta_data.get('微博认证')
+        info_items['label'] = meta_data.get('标签')
+        info_items['gender'] = meta_data.get('性别') 
+        info_items['place'] = meta_data.get('所在地')
+        info_items['profile'] = meta_data.get('简介')
+        info_items['school'] = meta_data.get('学校')
+        info_items['blog'] = meta_data.get('博客')
+        info_items['level'] = meta_data.get('等级')
+        info_items['credit'] = meta_data.get('阳光信用')
+        info_items['register'] = meta_data.get('注册时间')
+
+        yield info_items
