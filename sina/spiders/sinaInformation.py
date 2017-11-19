cf382bacf120d5a2a9b8d8a8582e172129626bba
apktool <apktoolOo@gmail.com>
2017-11-19 18:37:36 +0800

    Parse personal weibo home page

--- sina/spiders/sinaInformation.py
+++ sina/spiders/sinaInformation.py
@@ -1,4 +1,5 @@
 import scrapy
+import time
 
 
 class SinaSpider(scrapy.Spider):
@@ -6,13 +7,20 @@ class SinaSpider(scrapy.Spider):
     host = 'https://weibo.com/'
 
     def start_requests(self):
-        yield scrapy.Request(url=self.host, callback=self.parse)
+        yield scrapy.Request(url=self.host, callback=self.parse_home)
 
-    def parse(self, response):
+    def parse_home(self, response):
         cookie = response.request.headers.getlist('Cookie')
         print(cookie)
 
         self.logger.info('Parse function called on %s', response.url)
-        text = response.body
         with open('a.html', 'wb') as f:
-            f.write(text)
+            f.write(response.body)
+
+        info = response.xpath('//script/text()').re(r'(CONFIG.*?);')
+        CONFIG = dict()
+        for item in info[:-1]:
+            if "CONFIG['timeDiff']" in item:
+                item = item.replace('new Date()', str(int(time.time()*1000)))
+            exec(item)
+        self.logger.info('Parsing home page result is \n{}'.format(CONFIG))
