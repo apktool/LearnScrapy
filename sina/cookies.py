98ab442a83234c0d9a577259fdb0a4c47efb1cbe
apktool <apktoolOo@gmail.com>
2017-11-26 23:07:54 +0800

    Rewrite process of login of sina
    Note:
       I try to login weibo by url(weibo.cn), Because the data come from
       weibo.com are extracted hardly, I wouldn't like to spend too much
       time to solve this problem. The phone's web structure is simple,
       I think that I can save time by the way.
    
       Some question are existed by previous way which redirect url from
       weibo.com to weibo.cn, I don't know why the home page can't be got,
       Maybe I need to add refer into requests' headers

--- sina/cookies.py
+++ sina/cookies.py
@@ -1,163 +1,61 @@
 import requests
-import string
-import time
-import random
-import math
-import urllib
-import base64
-import hashlib
-import binascii
-import rsa
-import re
-
-s = requests.session()
-
-# https://login.sina.com.cn/signup/signin.php?entry=sso
 
 weibo = [
         {'no': 'account@sina.com', 'psw': 'password'},
     ]
 
-
-'''
 headers = {
-    'Host':'login.sina.com.cn',
-    'Origin':'https://login.sina.com.cn',
-    'Referer':'https://login.sina.com.cn/signup/signin.php?entry=sso',
-    'User-Agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/62.0.3202.75 Chrome/62.0.3202.75 Safari/537.36'
-}
-'''
+        'Referer': 'https://passport.weibo.cn/signin/login?entry=mweibo&r=http://weibo.cn/&backTitle=%CE%A2%B2%A9&vt=',
+    }
+
+s = requests.session()
+s.headers = headers
 
 
-def get_nonce():
-    s = ''
-    x = string.ascii_uppercase + string.digits
-    for i in range(6):
-        s += x[math.ceil(random.random() * 1000000) % len(x)]
-    return s
-
-
-def set_name(username):
-    b = urllib.parse.quote(username)
-    su = base64.b64encode(b.encode('utf-8'))
-    return su
-
-
-# sp: password = sinaSSOEncoder.hex_sha1("" + # sinaSSOEncoder.hex_sha1(sinaSSOEncoder.hex_sha1(password)) + me.servertime + me.nonce)
-def set_password(password, timestamp, nonce):
-    password_sha1_1 = hashlib.sha1(password.encode('utf-8')).hexdigest()
-    password_sha1 = password_sha1_1 + timestamp[:-3] + nonce
-    password_sha1_2 = hashlib.sha1(password_sha1.encode('utf-8')).hexdigest()
-    password_sha1_3 = hashlib.sha1(password_sha1_2.encode('utf-8')).hexdigest()
-    sp = password_sha1_3
-    return sp
-
-
-def get_sp(password, data):
-    servertime = str(data.get('servertime'))
-    nonce = data.get('nonce')
-
-    key = rsa.PublicKey(int(data.get('pubkey'), 16), int('10001', 16))
-
-    message = servertime + '\t' + nonce + '\n' + password
-    passwd = binascii.b2a_hex(rsa.encrypt(message.encode('utf-8'), key))
-    return passwd
-
-
-def pre_login(username, timestamp):
-    url = 'https://login.sina.com.cn/sso/prelogin.php'
-    params = {
-            'entry': 'sso',
-            'callback': 'sinaSSOController.preloginCallBack',
-            'su': username,
-            'rsakt': 'mod',
-            'client': 'ssologin.js(v1.4.15)',
-            '_': timestamp
+def login(username, password):
+    url = 'https://passport.weibo.cn/sso/login'
+    data = {
+            'username': username,
+            'password': password,
+            'savestate': '1',
+            'r': 'http://weibo.cn/',
+            'ec': '0',
+            'pagerefer': '',
+            'entry': 'mweibo',
+            'wentry': '',
+            'loginfrom': '',
+            'client_id': '',
+            'code': '',
+            'qq': '',
+            'mainpageflag': '1',
+            'hff': '',
+            'hfp': ''
             }
-    r = requests.get(url, params=params)
-    # content = r.content
-    content = eval(r.content.decode('utf-8').split('(')[1].split(')')[0])
-    return content
-
-
-def login(username, password, data):
-    login_url = 'https://login.sina.com.cn/sso/login.php?client=ssologin.js(v1.4.15)'
-    form = {
-            'entry': 'sso',
-            'gateway': '1',
-            'from': 'null',
-            'savestate': '30',
-            'useticket': '0',
-            'pagerefer': 'https://login.sina.com.cn/sso/login.php',
-            'vsnf': '1',
-            'su': username,
-            'service': 'sso',
-            'servertime': data.get('servertime'),
-            'nonce': data.get('nonce'),
-            'pwencode': 'rsa2',
-            'rsakv': data.get('rsakv'),
-            'sp': password,
-            'sr': '1920*1080',
-            'encoding': 'UTF-8',
-            'cdult': '3',
-            'domain': 'sina.com.cn',
-            'prelt': '30',
-            'returntype': 'TEXT'
-            }
-
-    r = s.post(login_url, data=form)
-    content = eval(r.content.decode('unicode-escape'))
-    # print(content['reason'])
-    return content
-
-
-def login2(content):
-    url = content.get('crossDomainUrlList')[0]
-    response = s.get(url.replace('\\', ''))
+    response = s.post(url, data=data)
     return response
 
 
-def login3(content):
-    url = content.get('crossDomainUrlList')[1]
-    response = s.get(url.replace('\\', ''))
-    return response
+def login_after(response):
+    url = response.get('data').get('crossdomainlist').get('weibo.com').replace('\\', '')
+    content = s.get(url)
+    print(content.status_code)
 
+    url = response.get('data').get('crossdomainlist').get('sina.com.cn').replace('\\', '')
+    content = s.get(url)
+    print(content.status_code)
 
-def login_cn():
-    url = 'https://weibo.cn'
-    body = s.get(url).text
-    addr = re.search(r'replace\("(.*?)"\);', body).group(1)
-    response = s.get(url=addr)
-    return response
+    url = response.get('data').get('crossdomainlist').get('weibo.cn').replace('\\', '')
+    content = s.get(url)
+    print(content.status_code)
 
 
 def get_cookies():
     cookies = []
     for item in weibo:
-        # nonce = get_nonce()
+        response = login(item.get('no'), item.get('psw'))
+        print(response.status_code)
 
-        username = set_name(item.get('no'))
-        timestamp = str(int(time.time()*1000))
-
-        data = pre_login(username, timestamp)
-        print(data)
-
-        # password = set_password(item.get('psw'), timestamp, nonce)
-        password = get_sp(item.get('psw'), data)
-        response = login(username, password, data)
-        print(response.get('retcode'))
-
-        response_2 = login2(response)
-        print(response_2.status_code)
-
-        response_3 = login3(response)
-        print(response_3.status_code)
-
-        response_cn = login_cn()
-        print(response_cn.status_code)
-
+        login_after(eval(response.text))
         cookies.append(s.cookies.get_dict())
 
     return cookies
-
-get_cookies()
