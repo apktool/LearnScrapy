ecaa92428ac28d5b4c2bfd2c06ab33c9fcb91941
apktool <apktoolOo@gmail.com>
2018-02-16 01:28:15 +0800

    Reconstruct scrapy releated to crawl weibo comment

--- sina/spiders/sina_personal_weibo_comment.py
+++ sina/spiders/sina_personal_weibo_comment.py
@@ -12,11 +12,14 @@ class SinaSpider(RedisSpider):
     name = "SinaPersonalWeiboComment"
     redis_key = 'sina_personal_weibo_comment:start_urls'
     start_urls = list(set(weibo_id))
-    url = 'https://m.weibo.cn/api/container/getIndex?'
+    basic_url = 'https://m.weibo.cn/api/comments/show?'
     params = dict()
+
     mongo_uri = str()
     mongo_db = str()
     mongo_collection = dict()
+
+    max_page = 10
 
     def connect_mongodb(self):
         settings = get_project_settings()
@@ -28,17 +31,23 @@ class SinaSpider(RedisSpider):
     def start_requests(self):
         self.connect_mongodb()
         for collection in self.mongo_collection:
-            for entry in collection.get('cards'):
+            for entry in collection.get('data').get('cards'):
                 mid = entry.get('mblog').get('mid')
-                for page in range(10):
-                    comment_url = 'https://m.weibo.cn/api/comments/show?id=' + mid + '&page=' + str(page)
-                    yield scrapy.Request(url=comment_url, meta={'mid': mid}, callback=self.parse_personal_weibo_comment)
+                temp = self.basic_url + 'id=%s&page=%d' % (mid, 1)
+                yield scrapy.Request(url=temp, meta={'mid': mid}, callback=self.parse_personal_weibo_comment)
+                self.continue_request(mid)
+
+    def continue_request(self, mid):
+        for page in range(1, self.max_page):
+            comment_url = self.basic_url + 'id=%s&page=%d' % (mid, page)
+            yield scrapy.Request(url=comment_url, meta={'mid': mid}, callback=self.parse_personal_weibo_comment)
 
     def parse_personal_weibo_comment(self, response):
         self.logger.info('Parse function called on %s', response.url)
 
         error_request_item = ErrorRquestItem()
         if response.status == 418 or response.status == 404:
+            error_request_item['_id'] = 'comment#%s' % (response.meta.get('mid'))
             print(response.url)
             ids = self.params['luicode'] + '#' + response.url.split('=')[-1]
             error_request_item['_id'] = ids
@@ -49,10 +58,14 @@ class SinaSpider(RedisSpider):
 
         jsonresponse = json.loads(response.body_as_unicode())
         personal_weibo_comment_item = PersonalWeiboCommentItem()
-        personal_weibo_comment_item['_id'] = str(response.meta.get('mid')) + '#' + str(response.url.split('=')[-1])
+        personal_weibo_comment_item['_id'] = response.meta.get('mid') + '#' + response.url.split('=')[-1]
         personal_weibo_comment_item['msg'] = jsonresponse.get('msg')
         personal_weibo_comment_item['data'] = jsonresponse.get('data')
         personal_weibo_comment_item['ok'] = jsonresponse.get('ok')
 
-        if personal_weibo_comment_item['ok'] == 1:
-            yield personal_weibo_comment_item
+        if personal_weibo_comment_item['ok'] == 0 or response.url.split('=')[-1] == '':
+            return
+        
+        self.max_page = jsonresponse.get('data').get('max')
+
+        yield personal_weibo_comment_item
